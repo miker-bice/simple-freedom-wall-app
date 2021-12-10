248192eedfa7f9509972de4287a36a4f3980f407
miker-bice <mikejerrson.galindez@gmail.com>
2021-12-10 15:01:17 +0800

    app models ready for migrations

--- freedomApp/models.py
+++ freedomApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Confessions(models.Model):
+    user_alias = models.CharField(max_length=15)
+    confession = models.CharField(max_length=2000)
+
