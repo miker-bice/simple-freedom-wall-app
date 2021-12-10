b79655730d5d5fbe8b056f6325d8435d662ee81e
miker-bice <mikejerrson.galindez@gmail.com>
2021-12-10 15:55:09 +0800

    templates for the app fixed

--- freedomApp/views.py
+++ freedomApp/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('hello World!')
+    return render(request, 'freedomApp/index.html')
 
 
