35beb8ee4cb2ca5466b870f52ce3a60ab6d5cb18
miker-bice <mikejerrson.galindez@gmail.com>
2021-12-10 15:26:08 +0800

    freedomApp views and urls added

--- freedomApp/models.py
+++ freedomApp/models.py
@@ -1,8 +1,14 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
-class Confessions(models.Model):
+class Confession(models.Model):
     user_alias = models.CharField(max_length=15)
     confession = models.CharField(max_length=2000)
+
+
+    
 
