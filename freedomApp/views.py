ffabf903f90784aa000e5c69ba98e95e2c332a90
miker-bice <mikejerrson.galindez@gmail.com>
2021-12-10 16:14:20 +0800

    add confession feature added
    the function that saves new confession and user still awaits testing

--- freedomApp/views.py
+++ freedomApp/views.py
@@ -1,5 +1,7 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Confession
 
 
 # Create your views here.
@@ -7,3 +9,9 @@ def index(request):
     return render(request, 'freedomApp/index.html')
 
 
+# This function saves new confessions made by the user
+def addConfessionView(request):
+    new_alias, new_confession = request.POST['user-alias', 'confession']
+    new_item = Confession(user_alias=new_alias, confession=new_confession)
+    new_item.save()
+    return HttpResponseRedirect('todoapp')
