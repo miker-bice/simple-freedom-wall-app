5e8487cc1ba829108dffeba9a9e4727c2b30602c
miker-bice <mikejerrson.galindez@gmail.com>
2021-12-10 16:42:02 +0800

    admin function added and fixed

--- freedomApp/admin.py
+++ freedomApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Confession
+
+
+class confessionAdmin(admin.ModelAdmin):
+    list_display = ('user_alias',)
+
 
 # Register your models here.
+admin.site.register(Confession, confessionAdmin)
