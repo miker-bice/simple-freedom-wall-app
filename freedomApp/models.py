d754b32b0a3a7ab00afdffb9273f680e9db405b3
miker-bice <mikejerrson.galindez@gmail.com>
2021-12-13 17:06:20 +0800

    Model updated
    timestamp field with auto_now_add added

--- freedomApp/models.py
+++ freedomApp/models.py
@@ -1,12 +1,11 @@
-import datetime
-
 from django.db import models
-from django.utils import timezone
+from datetime import datetime, date
 
 
 # Create your models here.
 class Confession(models.Model):
     user_alias = models.CharField(max_length=15)
     confession = models.CharField(max_length=2000)
-
+    timestamp = models.DateField(auto_now_add=True, auto_now=False, blank=True)
+    
 
