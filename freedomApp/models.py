815ee400a127a11b77339cdb7e83ef87fe9f6f08
miker-bice <mikejerrson.galindez@gmail.com>
2021-12-14 15:25:57 +0800

    Confession model updated
    the confession charfield is changed to textfield

--- freedomApp/models.py
+++ freedomApp/models.py
@@ -4,8 +4,8 @@ from datetime import datetime, date
 
 # Create your models here.
 class Confession(models.Model):
-    user_alias = models.CharField(max_length=15)
-    confession = models.CharField(max_length=2000)
+    user_alias = models.CharField(max_length=15, blank=False)
+    confession = models.TextField(max_length=2000, blank=False)
     timestamp = models.DateField(auto_now_add=True, auto_now=False, blank=True)
     
 
