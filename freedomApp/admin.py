027895c63fc0d215de78102dea5bb2215bd34b9b
miker-bice <mikejerrson.galindez@gmail.com>
2021-12-25 10:18:04 +0800

    Comment model registered to admin

--- freedomApp/admin.py
+++ freedomApp/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Confession
+from .models import Confession, Comment
 
 
 class confessionAdmin(admin.ModelAdmin):
     list_display = ('user_alias', 'confession', 'timestamp')
 
 
+class commentAdmin(admin.ModelAdmin):
+    list_display = ('commenter_name', 'comment_timestamp')
+
+
 # Register your models here.
 admin.site.register(Confession, confessionAdmin)
+admin.site.register(Comment, commentAdmin)
