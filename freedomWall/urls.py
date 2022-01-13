fee80e456d9e8beb14d6340a81a249df69b33703
miker-bice <mikejerrson.galindez@gmail.com>
2022-01-14 06:46:37 +0800

    admin url updated

--- freedomWall/urls.py
+++ freedomWall/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('not-the-admin/', admin.site.urls),
     path('freedom-app/', include('freedomApp.urls')),
 ]
 
