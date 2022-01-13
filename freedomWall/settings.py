d0cf00d75ee8e9bf1b04aa7d63395161eb9c571f
miker-bice <mikejerrson.galindez@gmail.com>
2022-01-14 06:35:44 +0800

    python-dotenv implemented

--- freedomWall/settings.py
+++ freedomWall/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os.path
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ze&erb9*i=!sf1d5%*k^)&bmz0!x6d54=nw%b@!xrz^-%634ln'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
